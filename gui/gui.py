faefca2c4aa94ceefd1f7b96461f79efcdac4376
neamaAhmed <37843612+neamaAhmed@users.noreply.github.com>
2019-04-27 05:14:14 +0200

    Add files via upload

---
+++ gui/gui.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 21 15:26:17 2019
+
+@author: ahmed
+"""
+
+
+
+from tkinter import *
+master = Tk() 
+Label(master, text='enter your pharagraph').grid(row=0) 
+e1 = Entry(master)  
+e1.grid(row=0, column=1)  
+mainloop()
+
+
+
+
+ 
